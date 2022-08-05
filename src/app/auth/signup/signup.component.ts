f128f4c533114ffa28feba93d6d032d40d97b742
Kirill <kirill.nikolaev@everon.fi>
2022-08-05 14:55:18 +0300

    spinner for signup

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { UIService } from 'src/app/shared/ui.service';
 import { AuthService } from '../auth.service';
 
 @Component({
@@ -7,13 +9,19 @@ import { AuthService } from '../auth.service';
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent implements OnInit, OnDestroy {
 
   maxDate: Date | undefined;
+  isLoading = false;
+  private loadingSubs: Subscription;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService,
+    private uiservice: UIService) { }
 
   ngOnInit(): void {
+    this.loadingSubs = this.uiservice.loadingStateChanged.subscribe(isLoading => {
+      this.isLoading = isLoading;
+    });
     this.maxDate = new Date();
     this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
   }
@@ -23,6 +31,10 @@ export class SignupComponent implements OnInit {
       email: form.value.email,
       password: form.value.password
     })
+  }
+
+  ngOnDestroy(): void {
+    this.loadingSubs.unsubscribe();
   }
 
 }
