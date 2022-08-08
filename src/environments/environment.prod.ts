40db981f9b11d94c01ad5a24a69f9ed625eb9b89
Kirill <kirill.nikolaev@everon.fi>
2022-08-08 11:09:03 +0300

    production changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA7mN3tG4QVqv1RiV00lW0SH37l6pv4qCw",
+    authDomain: "ng-fitness-tracker-7306d.firebaseapp.com",
+    projectId: "ng-fitness-tracker-7306d",
+    storageBucket: "ng-fitness-tracker-7306d.appspot.com",
+    messagingSenderId: "460401577587",
+    appId: "1:460401577587:web:6b28ddf121adcfdcd96406"
+  }
 };
